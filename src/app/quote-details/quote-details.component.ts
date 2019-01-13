206b256fe6ebce6df23b72830e28da2bfeb1e86e
abby-shabi <abbyshabi@gmail.com>
2019-01-13 15:32:24 +0300

    added the functionality for the upvote and downvote

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -12,3 +12,27 @@ export class QuoteDetailsComponent implements OnInit {
 
   @Output() isComplete= new EventEmitter<boolean>();
   @Output() toDelete = new EventEmitter<boolean>(); 
+  likeCount:number = 0;
+  dislikeCount :number =0;
+  
+  like(){
+    this.likeCount=this.likeCount +1
+  }
+  dislike (){
+    this.dislikeCount =this.dislikeCount +1
+  }
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+  quoteDelete(remove:boolean){
+    this.toDelete.emit(remove);
+  }
+  
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+}
